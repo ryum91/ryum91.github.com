d85e8142f9eae0fffcf8bb91866735344b324318
ryum91 <ryum91@gmail.com>
2017-10-17 19:56:40 +0900

    [Layout] Head Menu Fixed

---
+++ js/common.js
@@ -0,0 +1,14 @@
+
+var $topMenu = $('#topMenu');
+var $document = $(document);
+var offset = $topMenu.offset();
+var $topMenuFix = $('#topMenuFix');
+$(window).scroll(function() {
+  if( $document.scrollTop() > offset.top ) {
+    $topMenuFix.show();
+  } else {
+    $topMenuFix.hide();
+  }
+});
+
+$topMenuFix.css('width', $('.list-main').outerWidth());
